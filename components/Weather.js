44f72f67dcd806af90d6f13dfd0bc9020cf298aa
6310110029 <6310110029@psu.ac.th>
2022-08-27 22:57:44 +0700

    part5

--- components/Weather.js
+++ components/Weather.js
@@ -1,43 +1,46 @@
-import React from "react";
-import { Text, ImageBackground, StyleSheet } from "react-native"
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Text, View, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast';
+import Constants from 'expo-constants';
 
 export default function Weather(props){
 
   const [forecastInfo, setForecastInfo] = useState({
-        main: "-",
-        description: "-",
+        line: '',
+        main: '',
+        description: '',
         temp: 0})
 
     useEffect(() => {
     console.log(`fetching data with zipCode = ${props.zipCode}`)
     if (props.zipCode) {
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=1b6139f8d9343dbd67dd3c93c90aae2c`)
-        .then((response) => response.json())
-        .then((json) => {
-            setForecastInfo({
+         .then((response) => response.json())
+         .then((json) => {
+             setForecastInfo({
                 main: json.weather[0].main,
                 description: json.weather[0].description,
                 temp: json.main.temp
-            });
+              });
         })
-       .catch((error) => {
-          console.warn(error);
-        });
-    }
-}, [props.zipCode])
+        .catch((error) => {
+            console.warn(error);
+            });
+            }
+    }, [props.zipCode])
 
     
     
 
     return (
+        <View>
            <ImageBackground source={require('../cloud.jpg')} style={styles.backdrop}>
-                <Text>Zip code</Text>
-                <Text>{props.zipCode}</Text>
-                <Forecast {...forecastInfo}/>
-            </ImageBackground> 
+                <View style={styles.highlight}>
+                    <Text style={styles.titleText}>Zip code is {props.zipCode}.</Text>
+                    <Forecast {...forecastInfo}/>
+                </View>
+            </ImageBackground>
+            </View>
     );
 }
 
@@ -50,5 +53,13 @@ const styles = StyleSheet.create({
         //alignItems: "center",
         width: "100%",
         height: "100%"
+    },
+
+    highlight: {
+        backgroundColor: 'rgba(0, 255, 99, 0.5)',
+        width:"70%", 
+        height:"50%", 
+       // paddingTop: Constants.statusBarHeight, 
+        alignItems: 'center'
     },
 });
